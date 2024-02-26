f047546427d8e19eb5365a882dfd889e963070a3
denys <work.deniell90@gmail.com>
2024-02-26 23:32:21 +0200

    Homework 2

---
+++ js_basic_task1/task1.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+console.log(chalk.greenBright('Hello world!'));
+console.log(chalk.magenta('Hello world!'));
+console.log(chalk.redBright('Hello world!'));
+
---
+++ js_basic_task1/task2.js
@@ -0,0 +1,6 @@
+let countDays = 3
+const firstName = "Alex"
+let isRightJob = true;
+
+console.log(`His name is ${firstName}. He's been working on it for ${countDays} days`)
+console.log(isRightJob)
