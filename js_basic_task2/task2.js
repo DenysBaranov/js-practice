6e62512e19ac1261a38628b2e658f15dad268f87
denys <work.deniell90@gmail.com>
2024-03-03 19:30:39 +0200

    Edit task2.js file

--- js_basic_task2/task2.js
+++ js_basic_task2/task2.js
@@ -1,12 +1,12 @@
 let basicCost = 10000;
 let addedCost;
-let finalPrice;
+let finalPrice = basicCost;
 
-let carYear = 2021
+let carYear = 2021;
 let carMileage = 70000;
-let hasClimate = "Yes";
-let securityPack = "Yes";
-let electricCar = "Yes";
+let hasClimate = true;
+let securityPack = true;
+let electricCar = true;
 
 let message = "Car does not meet the stated criteria for changing the base price";
 
@@ -14,10 +14,9 @@ let message = "Car does not meet the stated criteria for changing the base price
 // If the car year is from 2019 to 2022 + 10% to the price 
 if (carYear>=2019 && carYear<=2022){
     addedCost = ((basicCost/100)*10);
-    finalPrice = (basicCost + addedCost);
+    finalPrice += addedCost;
 
     console.log(`1. Base price was ${basicCost}, if car has year-made 2019-2022 +${addedCost} to the final price`);
-    console.log(`Final price is ${finalPrice}`);
 } else {
     console.log(`1. ${message}`);
 }
@@ -26,10 +25,9 @@ if (carYear>=2019 && carYear<=2022){
 // If the car's mileage is from 50,000 km to 100,000 km - 5% of the price
 if (carMileage>=50000 && carMileage<=100000){
     addedCost = ((basicCost/100)*5);
-    finalPrice = (basicCost - addedCost);
+    finalPrice -= addedCost;
 
     console.log(`2. Base price was ${basicCost}, if car has mileage from 50,000 km to 100,000 km -${addedCost} from the final price`);
-    console.log(`Final price is ${finalPrice}`);
 } else{
     console.log(`2. ${message}`); 
 }
@@ -38,10 +36,9 @@ if (carMileage>=50000 && carMileage<=100000){
 // If the car has climate control + 7% to the price
 if(hasClimate){
     addedCost = ((basicCost/100)*7);
-    finalPrice = (basicCost + addedCost);
+    finalPrice += addedCost;
 
     console.log(`3. Base price was ${basicCost}, if car has climate control +${addedCost} to the final price`);
-    console.log(`Final price is ${finalPrice}`);
 } else{
     console.log(`3. ${message}`);
 }
@@ -50,10 +47,9 @@ if(hasClimate){
 // If the car with an additional security package +8% to the price
 if(securityPack){
     addedCost = ((basicCost/100)*8);
-    finalPrice = (basicCost + addedCost);
+    finalPrice += addedCost;
     
     console.log(`4. Base price was ${basicCost}, if car with an additional security package +${addedCost} to the final price`);
-    console.log(`Final price is ${finalPrice}`);
 } else{
     console.log(`4. ${message}`);
 }
@@ -62,15 +58,14 @@ if(securityPack){
 // If an electric car +15% to the price
 if(electricCar){
     addedCost = ((basicCost/100)*15);
-    finalPrice = (basicCost + addedCost); 
+    finalPrice += addedCost; 
 
     console.log(`5. Base price was ${basicCost}, if the car is an electric car +${addedCost} to the final price`);
-    console.log(`Final price is ${finalPrice}`);
 } else{
     console.log(`5. ${message}`);
 }
 
-
+console.log(`Final price is ${finalPrice}`);
 
 
 
